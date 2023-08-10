0e35cefb77d7c6e50998e2dd251f51d2a1b25c65
Aaron Bracht <aaron.bracht@tuhh.de>
2023-08-10 15:59:39 +0200

    Finished commenting except for mains

--- InterOperability/FnEstODE.m
+++ InterOperability/FnEstODE.m
@@ -1,13 +1,20 @@
 function ode = FnEstODE(trace)
+% FnEstODE estimates the ODE for each cluster in the trace datastructure
 global Ts num_var num_ud offsetCluster
+
+% Iterate over all clusters
 len_labels = length(trace(1).labels_num);
 for label = 1:len_labels
     max_num_var = num_var * (1 + offsetCluster);
+    % If higher order ODEs are estimated, derivatives need to be included
+    % in the state vector. But ODE degree unknown, thus searched greedy
     for off = offsetCluster:-1:0
         x_seg = [];
         x_seg_plus = [];
         ud_seg = [];
         num_vars = num_var * (1+off);
+        % Extract all segments from all traces associated with current
+        % cluster id to save state (and input) vector
         for j = 1:length(trace)
             labels_trace = trace(j).labels_trace;
             idx = find(labels_trace == label);
@@ -18,25 +25,33 @@ for label = 1:len_labels
             endj = trace(j).chpoints(idx+1)-1;
             
             for n = 1:length(startj)
+                % Save state space vector
                 x_seg = [x_seg, x(startj(n):(endj(n)-1), :)'];
+                % If no input available, use 1s as placeholders
                 if isempty(ud)
                     ud_seg = [ud_seg, ones(1, (endj(n)- startj(n)))];
                 else
                     ud_seg = [ud_seg, [ud(startj(n):(endj(n)-1), :)'; ones(1, (endj(n)- startj(n)))]];
                 end
+                % Save state space vector delayed by one timestep as 
+                % discrete state space representation used
                 x_seg_plus = [x_seg_plus, x((startj(n)+1):endj(n), :)'];
             end
         end
+
+        % ODE datastructure is A appended by b. As different ODE degrees
+        % are supported, A and b are extended to their maximal sizes
         
+        % No conversion to a continous model possible, denoted by -1s in ODE
         if size(x_seg,2)/length(trace)==2
-            %b is not estimated
+            % No inputs provided, thus b set to 0s
             if num_ud == 0
                 A_Bu = mrdivide(x_seg_plus,[x_seg]);
                 dA = A_Bu(:,1:num_vars);
                 dB = zeros(size(A_Bu,1),1);
                 % -1 indicate the ode is descrete and is used for variable reset
                 ode(label) = {[fillToSize(dA,[max_num_var max_num_var]), fillToSize(dB,[max_num_var 1]), -ones(num_vars,1)]};
-            %b is estimated
+            % Inputs provided, thus b properly estimated
             else
                 A_Bu = mrdivide(x_seg_plus,[x_seg;ud_seg]);
                 dA = A_Bu(:,1:num_vars);
@@ -47,7 +62,7 @@ for label = 1:len_labels
             return
         end
         
-        %b is not estimated
+        % No inputs provided, thus b set to 0s. Convert to continous model
         if num_ud == 0
             A_Bu = mrdivide(x_seg_plus,[x_seg]);
             dA = A_Bu(:,1:num_vars);
@@ -57,11 +72,14 @@ for label = 1:len_labels
             try
                 sysc = d2c(sysd,"zoh");
             catch
-                continue; %try with less derivatives included
+                % Conversin to continous model not possible because too
+                % many derivatives included in state vector, retry with less
+                continue;
             end
             ode(label) = {[fillToSize(sysc.A,[max_num_var max_num_var]), fillToSize(sysc.B,[max_num_var 1])]};
-            break; %ode found
-        %b is estimated
+            break;
+        
+        % Inputs provided, thus b properly estimated. Convert to continous model
         else
             A_Bu = mrdivide(x_seg_plus,[x_seg;ud_seg]);
             dA = A_Bu(:,1:num_vars);
@@ -71,16 +89,19 @@ for label = 1:len_labels
             try
                 sysc = d2c(sysd,"zoh");
             catch
-                continue; %try with a less derivatives included
+                % Conversin to continous model not possible because too
+                % many derivatives included in state vector, retry with less
+                continue;
             end
             ode(label) = {[fillToSize(sysc.A,[max_num_var max_num_var]), fillToSize(sysc.B,[max_num_var 1])]};
-            break; %ode found
+            break;
         end
     end
 end
 end
 
 function A_new = fillToSize(A,len)
+% fillToSize extends the matrix A to size len x len
     A_new = zeros(len);
     A_new(1:size(A,1),1:size(A,2)) = A;
 end
--- InterOperability/FnEvalCluster.m
+++ InterOperability/FnEvalCluster.m
@@ -1,41 +1,55 @@
 function [correct, false] = FnEvalCluster(estimated, truth, truth_chps)
+% FnEvalCluster evalautes the generated clusters against the ground truth 
+% clusters provided in truth. Because truth includes segments which are 
+% too short to detect, all changepoints of truth are provided to filter these  
     global windowSize
-    %remove too small segments out of truth array; TODO: Which threshold from codebase to use
+
+    % Remove too small segments out of truth array
     indx_remove = find((truth_chps(2:end) - truth_chps(1:(length(truth_chps)-1))) <= windowSize);
     truth(indx_remove) = [];
 
-    %find fitting permutation, both directions used because, e.g., T2E masks 
-    %if there are not enough states in estimated (and vice-versa)  
+    % Find fitting permutation between generated and truth cluster ids.
+    % Generate mapping truth to generated cluster ids, but this masks if  
+    % there are not enough generated clusters  
     labels_truth = unique(truth);
     M_T2E = containers.Map('KeyType','double','ValueType','double');
     for i = 1:length(labels_truth)
+        % Find all segments in truth with i'th cluster id
         indx = find(truth==labels_truth(i));
-        %remove indx that are out of bounds for truth array
+        % Remove indexes that are out of bounds for generated
         indxOutOfBounds = find(indx > size(estimated,1));
         indx(indxOutOfBounds) = [];
+        % Associate generated cluster id which occurs most often at these segments
         M_T2E(labels_truth(i)) = mode(estimated(indx));
     end
 
+    % Generate mapping generated to truth cluster ids, but this masks if  
+    % there are too many generated clusters  
     labels_est = unique(estimated);
     M_E2T = containers.Map('KeyType','double','ValueType','double');
     for i = 1:length(labels_est)
+        % Find all segments in generated with i'th cluster id
         indx = find(estimated==labels_est(i));
-        %remove indx that are out of bounds for truth array
+        % Remove indexes that are out of bounds for truth
         indxOutOfBounds = find(indx > size(truth,1));
         indx(indxOutOfBounds) = [];
+        % Associate truth cluster id which occurs most often at these segments
         M_E2T(labels_est(i)) = mode(truth(indx));
     end
 
-    %decide correct false incl. some edge cases
+    % Evaluate sequence of generated cluster ids against ground truth using
+    % both mappings to overcome their respective limitations
     correct = 0;
     false = 0;
     for i = 1:max(size(estimated,1),size(truth,1))
         if(i > size(truth,1) || i > size(estimated,1))
-            %sometimes estimated filled with zeros at end
+            % Too many/few segments in sequence of generated cluster ids
+            % Exclude trailing zeros as they are placeholders
             if(i <= size(estimated,1) && estimated(i,1) ~= 0)
                 false = false + 1;
             end
         elseif(estimated(i,1) == M_T2E(truth(i,1)) && M_E2T(estimated(i,1)) == truth(i,1))
+            % Both mappings indicate correct result
             correct = correct + 1;
         else
             false = false + 1;
